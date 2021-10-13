c159cbd605ac7417f4031b46c7578b97421892ce
Jordyn Price <88625349+JordynAntonia@users.noreply.github.com>
2021-10-13 09:04:19 -0400

    commit

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const root = document.getElementById('root');
-const container = document.createElement("div");
+const container = document.createElement(`div`);
 root.appendChild(container);
-container.classList.add("uk-container-large", "uk-flex", "uk-flex-between");
+container.classList.add(`uk-container-large`, `uk-flex`, `uk-flex-between`);
 
 for (let customer of customers) {
-    const customerCard = document.createElement("div");
+    const customerCard = document.createElement(`div`);
     customerCard.id = `${customer.id}`;
     container.appendChild(customerCard);
     customerCard.classList.add(
@@ -19,9 +19,30 @@ for (let customer of customers) {
 <img src=${customer.image}/>
 <p> class="uk-card-name">${customer.name}</p>
 <p>${customer.email}</p>
-<p>${customer.email}
-<p>${customer.address}
-<p>${customer.DOB}
-<p${customer.registration}<p>
+<p>${customer.address}</p>
+<p>DOB:${customer.dateofBirth}</p>
+<p>Customer since${customer.registration}</p>
 
 }
+
+for(var i = 0; i < 10; i++) {
+    document.body.innerHMTL += `
+}
+
+console.log = (customers[0].name.first.last)
+// <!-- // "Sophia Burns" -->
+
+console.log = (customers[4].email)
+// <!-- // 'sophia.burns@example.com' -->
+
+console.log = (customers[1].location.street)
+// <!-- // 4339 Green Rd -->
+
+console.log = (customers[1].location.city.state)
+// <!-- // Great Falls Alabama  -->
+
+console.log = (customers[4].dob.date)
+// <!-- // '1990-07-28T13:05:47.042Z' -->
+
+console.log = (customers[5].registered.date)
+// <!-- // '2012-03-09T06:54:45.213Z' -->
