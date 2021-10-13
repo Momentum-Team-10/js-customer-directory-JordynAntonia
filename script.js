c3392849e812ee2264b335b2fa6f7f123a5b5aac
Jordyn Price <88625349+JordynAntonia@users.noreply.github.com>
2021-10-13 10:11:25 -0400

    commit

--- script.js
+++ script.js
@@ -20,29 +20,8 @@ for (let customer of customers) {
 <p> class="uk-card-name">${customer.name}</p>
 <p>${customer.email}</p>
 <p>${customer.address}</p>
-<p>DOB:${customer.dateofBirth}</p>
-<p>Customer since${customer.registration}</p>
+<p>${moment(customer.dob.date).format("MM DD,YYYY")}</p>
+<p>Customer since:${customer.registration}</p>
 
-}
 
-for(var i = 0; i < 10; i++) {
-    document.body.innerHMTL += `
-}
 
-console.log = (customers[0].name.first.last)
-// <!-- // "Sophia Burns" -->
-
-console.log = (customers[4].email)
-// <!-- // 'sophia.burns@example.com' -->
-
-console.log = (customers[1].location.street)
-// <!-- // 4339 Green Rd -->
-
-console.log = (customers[1].location.city.state)
-// <!-- // Great Falls Alabama  -->
-
-console.log = (customers[4].dob.date)
-// <!-- // '1990-07-28T13:05:47.042Z' -->
-
-console.log = (customers[5].registered.date)
-// <!-- // '2012-03-09T06:54:45.213Z' -->
